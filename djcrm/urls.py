1801dccb372fd98f01bc2ba4e172880e463782b6
John Ketterer <johnmketterer@gmail.com>
2021-04-26 19:23:52 -0500

    password reset logic

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -5,20 +5,26 @@ from django.urls import path, include
 # from agents.views import 
 from leads.views import landing_page, LandingPageView, SignupView
 from django.contrib.auth.views import (
-    PasswordResetView, PasswordResetDoneView,
-    LoginView, LogoutView
+    PasswordResetView,
+    PasswordResetConfirmView, 
+    PasswordResetDoneView,
+    PasswordResetCompleteView,
+    LoginView,
+     LogoutView
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'), 
     # path('', landing_page, name='landing-page'),
-    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('leads/', include('leads.urls', namespace='leads')),
     path('agents/', include('agents.urls', namespace='agents')),
-
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
 
